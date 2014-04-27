57a55f99a40d6e48d56b19e7bcfc62855f722fa3
beFaust <jorges119@hotmail.com>
2014-04-27 17:39:47 +0100

    Added Comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,11 +1,12 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+# Here a new matrix object is created that is able to store and restore 
+# the calculated inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-  m <- NULL
-  set <- function(y) {
+  m <- NULL      
+  set <- function(y) {        
     x <<- y
     m <<- NULL
   }
@@ -18,7 +19,9 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+# This function gets the matrix object create by makeCacheMatrix and checks if the 
+# inverse has already been calculated
+# if this is the case it gets it from meory, otherwise calculates it and stores it.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
